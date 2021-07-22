32e8b6fd973a3f507632550cf0b45556c918ede4
BHAWNA GUPTA <48981152+BHAVI2803@users.noreply.github.com>
2021-07-21 17:18:50 -0700

    Update app.py

--- app.py
+++ app.py
@@ -179,7 +179,7 @@ def joint():
         sub_grad_value = int(sub_grad_model.predict(np.reshape(Fico_score, (1, -1)))[0])
 
         # loading csv file for interset rate
-        grade_to_apr_df = pd.read_csv('https://media.githubusercontent.com/media/BHAVI2803/LENDING_CLUB_DEPLOYMENT/main/data/grade_to_apr.csv',error_bad_lines=False)
+        grade_to_apr_df = pd.read_csv('data/grade_to_apr.csv')
         if term == 0:
             interest_rate = round((grade_to_apr_df[grade_to_apr_df['grade_num'] == sub_grad_value]['36_mo'].values[0]) / 100,2)
             # calculating EMI
